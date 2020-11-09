6bd35d06383391129ae625eccf2c049444fcb494
Collin Fairlie <collin.fairlie@gmail.com>
2020-11-09 14:18:35 -0600

    Filled in gaps with minimal code; left TODOs where revisions are needed from Prompter class addition.

--- src/com/game/dealornodeal/Game.java
+++ src/com/game/dealornodeal/Game.java
@@ -3,8 +3,10 @@ package com.game.dealornodeal;
 import com.game.dealornodeal.user.Prompter;
 
 // TODO: create a game constructor that takes a prompter and initilizes fields
-// TODO: put all prompts/dialog in the prompter class
+// DONE: put all prompts/dialog in the prompter class
 // DONE: create a prompter and main class
+// TODO: Revise all class methods that were once prompts and now rely on the Prompter class, look for notes throughout
+
 class Game {
     Board board = new Board();
     int[] eliminationRounds = {10, 5, 5, 3, 1};
@@ -16,30 +18,52 @@ class Game {
 
 
     public void playGame() {
-//        while(board.size > 1){
-//            For ( i = 0, i < eliminationRounds.length, i++){
-//                playRound(eliminationRounds[i])
-//            }
-//        }
+        // may need to create a way to break out out of certain loops if player chooses to
+        // accept bankOffer at any given point - maybe not. Tests will reveal this.
+        while (board.board.size() > 1) {
+            for (int i = 0; i < eliminationRounds.length; i++) {
+                playRound(eliminationRounds[i]);
+            }
+        }
         // call finalRound()
+        finalRound();
     }
 
     public void choosePlayerCase() {
-        // call player.chooseCase()
-        playGame();
+        // chosenCase will call the prompter to get the case number and save it
+        int chosenCase = prompter.chooseCase();
+        // check if chosenCase is in the 1-26 range
+        if (chosenCase >= 1 && chosenCase <= 26) {
+            System.out.println(player.getName() + " has chosen " + chosenCase + " to hold onto through each round.");
+            // TODO: revise player.chooseCase() to take in int chosenCase and find the case needed to save to player
+            // all prompt work is now done in the Prompter class
+            // call player.chooseCase()
+            playGame();
+        }
+        // give invalid message and continue to call method until valid entry has been met
+        else {
+            System.out.println(chosenCase + " is an invalid entry. Choice must be between 1-26.");
+            choosePlayerCase();
+        }
     }
 
 
     public void playRound(int numberOfEliminations) {
             // create loop for the round with numberOfEliminations
-                // TBD
-                // player chooses a case
-                // host grabs case with case number
-                // Case is presented to player
-
-
+        for (int i = 0; i < numberOfEliminations; i++) {
+            // TBD
+            // call prompter.askCaseChoice(player.getName()) to prompt player to choose case between 1-26
+            int chosenCase = prompter.askCaseChoice(player.getName());
+            // TODO: revise host.grabCase() to take in chosenCase from prompter
+            // call host.grabCase(chosenCase)
+            // all checks for valid number and search for case is done in other classes - Host and Board
+            // TODO: insure that user returns to playRound at same i "count" if entry is invalid and they need to call another number
+            // TODO: revise revealCase in Host to use proper name and not host name - this may require name be passed in from here when calling grabCase() above
+            // Case is presented to player in Host
+        }
             // call host.callBanker()
-            // call player.dealOrNoDeal()
+            String dealOrNoDealResponse = prompter.dealOrNoDeal();
+            // TODO: revise player.acceptOrDeclineDeal to take in dealOrNoDealResponse and proceed accordingly
 
     }
 
