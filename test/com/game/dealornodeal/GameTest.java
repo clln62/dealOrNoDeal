b6f45d9bce3102153bda6704bd777b7278a69671
Collin Fairlie <collin.fairlie@gmail.com>
2020-11-11 10:24:48 -0600

    init

--- test/com/game/dealornodeal/GameTest.java
+++ test/com/game/dealornodeal/GameTest.java
@@ -1,4 +1,33 @@
 package com.game.dealornodeal;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class GameTest {
+    Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
+    @Test
+    public void gameStart() {
+    }
+
+    @Test
+    public void choosePlayerCase() {
+    }
+
+    @Test
+    public void playGame() {
+    }
+
+    @Test
+    public void playRound() {
+    }
+
+    @Test
+    public void finalRound() {
+    }
 }
