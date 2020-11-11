7de5422149d1108901cf48196bc943246cd40253
Collin Fairlie <collin.fairlie@gmail.com>
2020-11-11 10:24:31 -0600

    Reformatting code.

--- test/com/game/dealornodeal/BankerTest.java
+++ test/com/game/dealornodeal/BankerTest.java
@@ -4,46 +4,44 @@ import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+
 import java.util.NoSuchElementException;
+
 import static org.junit.Assert.*;
 
-@FixMethodOrder (MethodSorters.NAME_ASCENDING)
-public class BankerTest
-{
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class BankerTest {
     Banker banker;
     Board board;
     Player player;
 
     @Before
-    public void init()
-    {
+    public void init() {
         player = new Player();
         banker = new Banker();
         board = new Board();
     }
+
     @Test
-    public void calculateDealWithFullBoard_shouldReturnOfferAmount()
-    {
+    public void calculateDealWithFullBoard_shouldReturnOfferAmount() {
         assertNotNull(board);
-        assertEquals(424304.0,Banker.calculateDeal(player.getChosenCase()), 0.01);
+        assertEquals(424304.0, Banker.calculateDeal(player.getChosenCase()), 0.01);
     }
-    @Test(expected=NoSuchElementException.class)
-    public void calculateDealWithEmptyBoard_shouldReturnNoSuchElementException()
-    {
+
+    @Test(expected = NoSuchElementException.class)
+    public void calculateDealWithEmptyBoard_shouldReturnNoSuchElementException() {
         Board.board.clear();
         assertNotNull(board);
         Banker.calculateDeal(player.getChosenCase());
     }
 
     @Test
-    public void getOfferAmountWithFullBoard_shouldReturnOfferFromCalculateDeal()
-    {
-        assertEquals(424304.0,Banker.getOfferAmount(player.getChosenCase()), 0.01);
+    public void getOfferAmountWithFullBoard_shouldReturnOfferFromCalculateDeal() {
+        assertEquals(424304.0, Banker.getOfferAmount(player.getChosenCase()), 0.01);
     }
 
     @Test
-    public void getOfferAmountWithPartialBoard_shouldReturnOfferFromCalculateDeal()
-    {
+    public void getOfferAmountWithPartialBoard_shouldReturnOfferFromCalculateDeal() {
         Board.board.clear();
         Case case1 = new Case();
         case1.setCaseNumber(1);
@@ -57,6 +55,6 @@ public class BankerTest
         Board.board.add(case1);
         Board.board.add(case2);
         Board.board.add(case3);
-        assertEquals(656250.0,Banker.getOfferAmount(player.getChosenCase()), 0.01);
+        assertEquals(656250.0, Banker.getOfferAmount(player.getChosenCase()), 0.01);
     }
 }
