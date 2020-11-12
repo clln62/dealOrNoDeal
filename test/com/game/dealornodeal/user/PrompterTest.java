e3b9c2d761353ea9606e41bed33ad95e64aba348
Chris <chris.ruaboro@gmail.com>
2020-11-12 00:19:54 -0800

    Failed implementing Scanner and Prompter

--- test/com/game/dealornodeal/user/PrompterTest.java
+++ test/com/game/dealornodeal/user/PrompterTest.java
@@ -3,19 +3,25 @@ package com.game.dealornodeal.user;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.Assert.*;
 
 public class PrompterTest
 {
+    Scanner input;
 
     @Before
     public void setUp() throws Exception
     {
-
+        input = new Scanner(new File("responses/responses.txt"));
+        Prompter prompter = new Prompter();
     }
 
     @Test
-    public void askPlayerName()
+    public void askPlayerName() throws Exception
     {
 
     }
@@ -23,25 +29,30 @@ public class PrompterTest
     @Test
     public void askCaseChoice()
     {
+
     }
 
     @Test
     public void dealOrNoDeal()
     {
+
     }
 
     @Test
     public void presentCases()
     {
+
     }
 
     @Test
     public void askToSwap()
     {
+
     }
 
     @Test
     public void chooseCase()
     {
+
     }
 }
